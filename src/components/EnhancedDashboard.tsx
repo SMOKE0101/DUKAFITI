fa5da7e60079ef8f9734f670e00cf7e2fb53419a
AKYEGON <yegonalvin@gmail.com>
2025-08-19 14:33:13 +0300

    Deleted enhanced-robust-sw.js

--- src/components/EnhancedDashboard.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import ColoredCardDashboard from './ColoredCardDashboard';
-
-const EnhancedDashboard = () => {
-  return <ColoredCardDashboard />;
-};
-
-export default EnhancedDashboard;
